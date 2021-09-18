44a914c67186dd25544d59db603be38f5fc0cf4e
Tim <tim.cieplowski@gmail.com>
2021-09-18 17:50:53 -0400

    reduce pill size

--- components/SocialBig.vue
+++ components/SocialBig.vue
@@ -3,7 +3,7 @@
     <div class="md:flex flex-wrap align-middle justify-center gap-4 space-y-4 md:space-y-0">
       <div v-for="link, idx in $config.social_links" :key="idx">
         <a :href="link.url">
-          <div class="rounded-full py-3 px-6 bg-blue-fj text-white text-center hover:opacity-80">
+          <div class="rounded-full py-2 px-4 bg-blue-fj text-white text-center hover:opacity-80">
               <font-awesome-icon :icon="[link.icon[0], link.icon[1]]" />
               {{ link.name }}
           </div>
