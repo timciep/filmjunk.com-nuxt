e3952034fb03d5ec9ce745af45d45a5b840ff373
Tim <tim.cieplowski@gmail.com>
2021-09-21 15:05:33 -0400

    generate episode routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -182,7 +182,15 @@ export default {
 
 
   generate: {
-    fallback: true
+    fallback: true,
+    crawler: false,
+    // subFolders: false,
+
+    async routes() {
+      const { $content } = require('@nuxt/content');
+      const files = await $content({ deep: true }).only(['path']).fetch();
+      return files.map(file => file.path === '/index' ? '/' : file.path);
+    }
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
